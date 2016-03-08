03c1435c94b38a9876707b79557cb78fb9578a6f
muthamilanram10 <muthamilanram10@gmail.com>
2016-03-08 08:57:25 -0800

    Added files via upload

---
+++ assignment.operator.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int x,y,z;
+ clrscr();
+z =(x=4,y=5);
+printf("z =%d",z);
+getch();
+}
---
+++ relation.operator.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+printf("\n condition :return values \n");
+printf("\n 5!=5  : %5d",5!=5);
+printf("\n 5==5  : %5d",5==5);
+printf("\n 5>=50  : %5d",5>=50);
+printf("\n 5<=50 : %5d",5<=50);
+}
