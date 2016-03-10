5a8c5df9124bf3a23d82120a616f790fe8bea469
gbpnkans <gbpnkans@gmail.com>
2016-03-10 09:31:33 -0700

    Cleanup wrong filenaming convention
    ok

--- enumerator family.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-#include<string.h>
-void main()
-{
-char res[]="muthamilan";
-printf("%c",res[5]);
-printf("%s",res+5);
-}
--- receiveinput.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-#include<string.h>
-void main()
-{
- char res[50];
- gets(res);
-printf("%s",res);
-}
