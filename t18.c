7eecc3b314321801fbe8edd8e431be02ed5dc11a
muthamilanram10 <muthamilanram10@gmail.com>
2016-03-14 20:19:15 +0530

    Added files via upload

---
+++ t18.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+void main()
+
+{
+
+ int a,b=6,c=8,d=2;
+
+ int sum,sub,mul,rem;
+ 
+float div;
+ 
+sum=b+c;
+
+ sub=b-c;
+
+ mul=b*c;
+ 
+div=b/c;
+
+
+ printf("\n sum= %d ,sub=%d,mul=%d,rem=%d,div=%d\n\n",sum,sub,mul,rem,div);
+
+}
