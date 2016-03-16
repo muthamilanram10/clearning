768d1654dd557faa72860b4a6f8334fd3fc4a289
muthamilanram10 <muthamilanram10@gmail.com>
2016-03-16 20:24:50 +0530

    Added files via upload

---
+++ T37.C
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<string.h>
+void main()
+{
+char str1[20];
+char str2[30];
+printf("Enter name:");
+scanf("%s",str1);
+printf("Enter your department: ");
+scanf("%s",str2);
+printf("Entered Name: %s\n",str1);
+printf("Entered department:%s",str2);
+}
